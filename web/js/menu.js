ce8e799d2f6e274ee277248a9b2db8afacc260d6
FRnCDR <franyerifallasnichols@gmail.com>
2025-11-27 09:38:11 -0600

    Detalles para entrega 2
    Detalles para entrega 2

--- web/js/menu.js
+++ web/js/menu.js
@@ -3,11 +3,6 @@ const menus = `
     <nav class="mt-3">
     <ul class="nav nav-pills justify-content-center bg-white p-2 rounded shadow-sm">
         <li class="nav-item">
-            <a class="nav-link text-success fw-semibold" href="cursojq.html">
-                <i class="fa fa-code"></i> Curso jQ
-            </a>
-        </li>
-        <li class="nav-item">
             <a class="nav-link text-success fw-semibold" href="menu.html">
                 <i class="fa fa-bars"></i> Menú
             </a>
